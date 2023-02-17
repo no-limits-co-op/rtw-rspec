f6834a3bdc4ed5a89d9e1744dd07da5558cb8908
huzhuangzhuang <zhuangzhuang@dao42.com>
2023-02-17 08:36:13 +0800

    init project

---
+++ lib/rtw/rspec.rb
@@ -0,0 +1,8 @@
+require "rtw/rspec/version"
+
+module Rtw
+  module Rspec
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rtw/rspec/version.rb
@@ -0,0 +1,5 @@
+module Rtw
+  module Rspec
+    VERSION = "0.1.0"
+  end
+end
